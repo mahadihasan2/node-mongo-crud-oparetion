17e5beaf6a27bc11ed287b6af09cba808d6da487
mahadihasan2 <mahadihasan125643@gmail.com>
2022-05-12 01:25:17 +0600

    node-express-setUp-done

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
+
+app.get('', (req, res) => {
+    res.send('running my crud operations')
+})
+
+
+// Server Listening 
+app.listen(port, () => {
+    console.log('CRUD server is running', port);
+})
