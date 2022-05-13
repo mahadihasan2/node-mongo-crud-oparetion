c8346fee93c0d3685918a4ec948e755dd7cb9053
mahadihasan2 <mahadihasan125643@gmail.com>
2022-05-13 22:25:10 +0600

    delete user data for ui and server

--- index.js
+++ index.js
@@ -56,6 +56,8 @@ async function run() {
         app.delete('/user/:id', async (req, res) => {
             const id = req.params.id
             const query = { _id: ObjectId(id) }
+            const result = await userCollection.deleteOne(query)
+            res.send(result)
         })
     }
     finally {
