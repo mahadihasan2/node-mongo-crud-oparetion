63aebad881eb14009d9048921781f428bab365b4
mahadihasan2 <mahadihasan125643@gmail.com>
2022-05-13 20:29:03 +0600

    data get with server

--- index.js
+++ index.js
@@ -1,13 +1,66 @@
 const express = require('express')
-const app = express()
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('', (req, res) => {
+// Middleware use 
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
     res.send('running my crud operations')
 })
 
 
+// UserName : dbUser21
+// Password :RA213Q2abYAA1M7n
+
+// MongoDB DataBase  Conected to Server 
+
+
+
+const uri = "mongodb+srv://dbUser21:RA213Q2abYAA1M7n@cluster0.laant.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const userCollection = client.db('foodExpress').collection('user')
+        // const user = { name: 'Mahiya Mahi khan', email: 'mahiyamahikhan@gmail.com' }
+        // const result = await userCollection.insertOne(user);
+        // console.log(`User inserted id ${result.insertedId}`)
+
+        // Get data 
+
+        app.get('/user', async (req, res) => {
+            const query = {}
+            const cursor = userCollection.find(query)
+            const users = await cursor.toArray()
+            res.send(users)
+        })
+
+
+        // Post data and collection
+        app.post('/user', async (req, res) => {
+            const newUser = req.body
+            console.log('adding to new user', newUser)
+            const result = await userCollection.insertOne(newUser)
+            res.send(result)
+        })
+    }
+    finally {
+
+    }
+
+}
+
+run().catch(console.dir);
+
+
+
+
 // Server Listening 
 app.listen(port, () => {
-    console.log('CRUD server is running', port);
-})
+    console.log('CRUD server is running');
+});
